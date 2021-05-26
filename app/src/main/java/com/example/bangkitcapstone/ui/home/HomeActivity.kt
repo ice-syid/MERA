9e9253ea619c8d1464445eb74ef3cd9032f178ce
Abdurrasyid Muhasibi <ice.syid@gmail.com>
2021-05-26 17:11:46 +0700

    update LoginFragment

--- app/src/main/java/com/example/bangkitcapstone/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/bangkitcapstone/ui/home/HomeActivity.kt
@@ -1,11 +1,13 @@
 package com.example.bangkitcapstone.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bangkitcapstone.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        val binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/bangkitcapstone/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/bangkitcapstone/ui/login/LoginFragment.kt
@@ -1,18 +1,31 @@
 package com.example.bangkitcapstone.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.bangkitcapstone.R
+import com.example.bangkitcapstone.databinding.FragmentLoginBinding
+import com.example.bangkitcapstone.ui.home.HomeActivity
 
 class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding as FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(activity, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
