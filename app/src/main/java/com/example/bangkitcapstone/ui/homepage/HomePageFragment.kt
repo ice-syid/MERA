e5cae2d42f57c7f8f3117348a76b589a2a0fb0c6
Abdurrasyid Muhasibi <ice.syid@gmail.com>
2021-05-26 13:51:51 +0700

    add homepage

---
+++ app/src/main/java/com/example/bangkitcapstone/ui/homepage/HomePageFragment.kt
@@ -0,0 +1,29 @@
+package com.example.bangkitcapstone.ui.homepage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.bangkitcapstone.R
+
+class HomePageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home_page, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (requireActivity() as AppCompatActivity).supportActionBar?.show()
+    }
+}
