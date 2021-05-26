99104293fb785d1be93d0487fd72721043a2e980
Abdurrasyid Muhasibi <ice.syid@gmail.com>
2021-05-26 17:05:26 +0700

    update RegisterFragment

--- app/src/main/java/com/example/bangkitcapstone/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/example/bangkitcapstone/ui/register/RegisterFragment.kt
@@ -5,14 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.bangkitcapstone.R
+import androidx.navigation.findNavController
+import com.example.bangkitcapstone.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding as FragmentRegisterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.btnRegister.setOnClickListener {
+            val actionRegisterLogin =
+                RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
+            view.findNavController().navigate(actionRegisterLogin)
+        }
     }
 }
