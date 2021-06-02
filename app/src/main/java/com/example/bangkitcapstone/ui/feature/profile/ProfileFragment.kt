569fb86891d0fa9f3c4a0c630bf08bb1cbd47e23
Abdurrasyid Muhasibi <ice.syid@gmail.com>
2021-06-02 11:11:13 +0700

    update ProfileFragment

--- app/src/main/java/com/example/bangkitcapstone/ui/feature/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/bangkitcapstone/ui/feature/profile/ProfileFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.bangkitcapstone.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -20,6 +21,14 @@ class ProfileFragment : Fragment() {
         (requireActivity() as AppCompatActivity).supportActionBar?.hide()
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        with(binding) {
+            Glide.with(view.context)
+                .load("https://static.wikia.nocookie.net/solo-leveling/images/5/59/Jin-Woo_Profile.png/revision/latest/scale-to-width-down/340?cb=20200208070835")
+                .into(imgProfile)
+        }
     }
 
     override fun onResume() {
