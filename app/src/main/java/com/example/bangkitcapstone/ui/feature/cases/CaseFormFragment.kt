6bb2c7bc9d54eb21e01a8f338be1e5bf28a308be
Abdurrasyid Muhasibi <ice.syid@gmail.com>
2021-06-03 13:58:18 +0700

    add CaseFeedbackFragment

--- app/src/main/java/com/example/bangkitcapstone/ui/feature/cases/CaseFormFragment.kt
+++ app/src/main/java/com/example/bangkitcapstone/ui/feature/cases/CaseFormFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.bangkitcapstone.R
 import com.example.bangkitcapstone.databinding.FragmentCaseFormBinding
 
@@ -32,7 +33,14 @@ class CaseFormFragment : Fragment() {
             "Body Shaming",
             "Rasisme"
         )
+
         binding.spinnerCategory.adapter =
-            ArrayAdapter<String>(requireContext(), R.layout.spinner_item, options)
+            ArrayAdapter(requireContext(), R.layout.spinner_item, options)
+
+        binding.btnCreateReport.setOnClickListener {
+            val actionCreateNewReport =
+                CaseFormFragmentDirections.actionCaseFormFragmentToCaseFeedbackFragment()
+            view.findNavController().navigate(actionCreateNewReport)
+        }
     }
 }
