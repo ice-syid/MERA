578d01ee70b7fe569bb9b7cae1a6e745aa3e6f0e
Abdurrasyid Muhasibi <ice.syid@gmail.com>
2021-06-08 18:43:58 +0700

    add SplashScreen

---
+++ app/src/main/java/com/example/bangkitcapstone/ui/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.bangkitcapstone.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bangkitcapstone.R
+import com.example.bangkitcapstone.ui.main.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2500)
+    }
+}
